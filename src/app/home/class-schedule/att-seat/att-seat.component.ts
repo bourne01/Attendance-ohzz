8f416ab7c9cfe0f830d3ec329111573887a69201
Bourne <710150774@qq.com>
2018-01-03 14:37:01 +0800

    seat ..

--- src/app/home/class-schedule/att-seat/att-seat.component.ts
+++ src/app/home/class-schedule/att-seat/att-seat.component.ts
@@ -9,11 +9,25 @@ import { ClassScheduleService } from '../class-schedule.service';
   styleUrls: ['./att-seat.component.css']
 })
 export class AttSeatComponent implements OnInit {
+  private studentsList:any[][]=[];
   constructor(private studentService:ClassScheduleService) { }
 
   ngOnInit() {
     this.studentService.getStudents().subscribe(
-      data => console.log(data),
+      data => {
+        let _studentsList:any[][]=[];
+        let _studentList:any[] = data['dataList'];
+        let _studentList_:any[] = [];
+        /*把学生分成8个小组 */
+        for(let i=0;i<_studentList.length;i++){
+          _studentList_.push(_studentList[i]);
+          if(( i != 0 && (i % 8) == 7) || i == (_studentList.length-1)){
+            _studentsList.push(_studentList_);
+            _studentList_ = [];
+          }
+        }
+        this.studentsList = _studentsList.reverse();//反序输出数组，即序号小的同学坐第一排
+      },
       error => console.log(error)
     )
   }  
